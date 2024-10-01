7ca3517cbd43a08fd2198392f9342e23e9bbd321
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-01 18:30:22 +0530

    done

--- B_Longest_Divisors_Interval.cpp
+++ B_Longest_Divisors_Interval.cpp
@@ -2,20 +2,6 @@
 #define ll long long
 using namespace std ;
 
-vector<ll> findDivisors(ll n) {
-    vector<ll> divisors;
-    for (ll i = 1; i * i <= n; ++i) {
-        if (n % i == 0) {
-            divisors.push_back(i);
-            if (i != n / i) {
-                divisors.push_back(n / i);
-            }
-        }
-    }
-    sort(divisors.begin(), divisors.end());
-    return divisors ;
-}
-
 int main() {
 int t;
 cin >> t ;
@@ -24,24 +10,19 @@ while(t--) {
     ll n ;
     cin >> n ;
 
-    vector<ll> divisors = findDivisors(n) ;
+    if(n%2 != 0){
+    cout << "1" << endl ;
+    continue ;}
 
-    int len = 1 ;
-    int size = divisors.size() ;
-    int max_len = 1 ;
-
-   for(ll i=1;i<size;i++) {
-    if(divisors[i] - divisors[i-1] == 1)
-    len++ ;
-
-    else{
-        max_len = max(max_len,len) ;
-        len = 1 ;
+    int x = 0 ;
+    for(int i=1;i<n;i++) {
+        if(n%i != 0){
+        x = i ; 
+        break ;}
     }
-   }
+  
+    cout << x-1 << endl ;
 
-    max_len = max(max_len,len) ;
-    cout << max_len << endl ;
 }
 
     return 0 ;
