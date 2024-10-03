af60f4708d8814411a11b83fecb49d0171510798
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-04 04:55:07 +0530

    900

---
+++ B_Make_AP.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t;
+cin >> t ;
+
+while(t--) {
+    int a,b,c ;
+    cin >> a >> b >> c ;
+
+    if(((2 * b - c) > 0 && (2 * b - c) % a == 0) || ((a + c) % (2 * b) == 0) || ((2 * b - a) > 0 && (2 * b - a) % c == 0))
+    cout << "YES" << endl ; 
+
+    else 
+    cout << "NO" << endl ;
+}
+
+    return 0 ;
+}
