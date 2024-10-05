b26153b36fc3e48932fd598c838fd6cad8f741fb
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-06 00:42:27 +0530

    900

---
+++ A_Exciting_Bets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main() {
+int t ;
+cin >> t ;
+
+while(t--) {
+    long long a,b ;
+    cin >> a >> b ;
+
+    if(a == b)
+    cout << 0 << " " << 0 << endl ;
+
+    else {
+        long long maxgcd = abs(a-b) ;
+        cout << maxgcd << " " ;
+
+        long long ans1 = a%maxgcd ;
+        long long ans2 = abs(maxgcd-a%maxgcd) ;
+        cout << min(ans1,ans2) << endl ;
+    }
+}
+
+    return 0 ;
+}
