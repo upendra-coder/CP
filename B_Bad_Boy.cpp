025db4c4874642ece971bc5d0605f8229fca096b
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-06 01:58:20 +0530

    Create B_Bad_Boy.cpp

---
+++ B_Bad_Boy.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int t ;
+cin >> t ;
+
+while(t--) {
+    int n,m,icx,icy ;
+    cin >> n >> m >> icx >> icy ;
+    int x1,y1,x2,y2 ; 
+
+    cout << n << " " << m << " " <<  1 << " " << 1 << endl ;
+}
+
+    return 0;
+}
