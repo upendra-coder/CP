fa1cf4439c2d7d504fb1fd7cb0efa49be2a5fccc
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-20 20:21:02 +0530

    contest

--- A_Profitable_Interest_Rate.cpp
+++ A_Profitable_Interest_Rate.cpp
@@ -1,35 +1,64 @@
 #include <bits/stdc++.h>
-using namespace std ;
-
+using namespace std;
 int main() {
 ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-int t ;
-cin >> t ;
-
-while(t--) {
-    long long a,b ;
-    cin >> a >> b ;
-
-    if(a >= b) 
-        cout << a << endl ;
-
-    else {
-        long long slow = a ;
-        long long fast = b ;
-        
-        while(true){
-          if(slow >= fast) break ;
-          if(slow == 0) break ;
-          
-          slow -= 1 ;
-          fast -= 2 ;
-        }
-
-        if(slow == fast) cout << slow << endl ;
-        else cout << 0 << endl ;
+  long long t;
+  cin>>t;
+  
+  while(t--){
+    long long a,b;
+    cin >> a >> b;
+    
+    if(a>=b){
+      cout << a << endl;
     }
+    
+    else if(b>=2*a){
+      cout << "0" << endl;
+    }
+    
+    else{
+      cout << 2*a-b << endl;
+    } 
+  }
+return 0;
 }
-    return 0 ;
-}
+
+// TLE -> but approach is correct
+// #include <bits/stdc++.h>
+// using namespace std ;
+
+// int main() {
+// ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+// int t ;
+// cin >> t ;
+
+// while(t--) {
+//     long long a,b ;
+//     cin >> a >> b ;
+
+//     if(a >= b) 
+//         cout << a << endl ;
+
+//     else {
+//         long long slow = a ;
+//         long long fast = b ;
+        
+//         while(true){
+//           if(slow >= fast) break ;
+//           if(slow == 0) break ;
+          
+//           slow -= 1 ;
+//           fast -= 2 ;
+//         }
+
+//         if(slow == fast) cout << slow << endl ;
+//         else cout << 0 << endl ;
+//     }
+// }
+//     return 0 ;
+// }
+
+
 
 
