07bcd6127ff1517ae1e2eb714fd41692d052b87c
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2024-10-20 17:32:18 +0530

    contest_A

---
+++ A_Profitable_Interest_Rate.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main() {
+ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int t ;
+cin >> t ;
+
+while(t--) {
+    long long a,b ;
+    cin >> a >> b ;
+
+    if(a > b) 
+        cout << a << endl ;
+
+    else {
+        long long slow = a ;
+        long long fast = b ;
+        
+        while(true) {
+          if(slow == fast) break ;
+          if(slow == 0) break ;
+          
+          slow -= 1 ;
+          fast -= 2 ;
+        }
+
+        if(slow == fast) cout << slow << endl ;
+        else cout << 0 << endl ;
+    }
+}
+    return 0 ;
+}
+
+
