62ed9a7f064c34126c14f157a607f59caee0415d
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2025-01-22 18:55:00 +0530

    800

--- A_Strange_Splitting.cpp
+++ A_Strange_Splitting.cpp
@@ -8,12 +8,45 @@ while(t--){
     int n;  cin >> n;
 
     vector<int>a(n);
-    for(int i=0;i<n;i++)
-    cin >> a[i];
+    unordered_map<int,bool> mp;
+    int distinct = 0;
+    for(int i=0;i<n;i++){
+       cin >> a[i];
+       if(mp[a[i]] != true){ 
+        mp[a[i]] = true; distinct++ ;
+        }
+    }
 
-    
+    if(distinct == 1){
+        cout << "NO" << endl;
+        continue;
+    }
+
+    else if(distinct == n){
+        cout << "YES" << endl;
+        cout << "R";
+        for(int i=1;i<n;i++){
+            cout << "B" ;
+        }
+        cout << endl;
+    }
+
+    else{
+        cout << "YES" << endl;
+        cout << "R";
+        int i=1;
+        while(a[i] != a[i-1] && i < n){
+            cout << "R";
+            i++ ;
+        }
+
+        for(i;i<n;i++){
+            cout << "B";
+        }
+        cout << endl;
+        
+    }
 }
-
 
     return 0;
 }
