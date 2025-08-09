f034324c9e3c6ee010050a6de14eae6f5ecb150b
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2025-08-09 16:29:11 +0530

    900

---
+++ A_Painting_the_Ribbon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t;  cin >> t;
+
+while(t--){
+    int n,m,k;  cin >> n >> m >> k;
+    
+    int max_color = (n+m-1)/m;
+    if(max_color + k >= n) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
+
+    return 0;
+}
